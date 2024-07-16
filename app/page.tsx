f1edcb82573f33230f2a724f8f90aa39da66b4d0
Phunmehkhe <adeyeyeoluwafunmike1@gmail.com>
2024-07-16 21:39:47 +0100

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ const Home = () => {
       <div className="container mx-auto h-full my-12 bg-secondary">
         <div className="flex flex-col items-center justify-center px-2 text">
           <h2 className='font-extrabold py-8 w-[1000px] text-center text-6xl text-primary'>Simplify Your Business Expenses with ExpenseXpress</h2>
-          <p className="text-center px-4 py-4 w-[700px] text-xl">Transform your financial operations with ExpenseXpress. Stay on top of your finances with ease, whether you're managing invoices, or tracking expenses. </p>
+          <p className="text-center px-4 py-4 w-[700px] text-xl">Transform your financial operations with ExpenseXpress. Stay on top of your finances with ease, whether you&apos;re managing invoices, or tracking expenses. </p>
           <div className="flex gap-6 py-6 text-xl">
             <button className="bg-primary px-4 py-2 rounded-[20px] hover:bg-secondary hover:border hover:border-white">Sign in</button>
             <button className="bg-secondary border border-white px-4 py-2 rounded-[20px] hover:bg-primary hover:border-none">Sign up</button>
